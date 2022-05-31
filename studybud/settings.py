4bdf856909a3affe30371e082bf316487bdf92fe
190030201 <73651038+Srivathsav-max@users.noreply.github.com>
2022-05-31 08:32:47 +0530

    -

--- studybud/settings.py
+++ studybud/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=+f6rs9&9^h+=j29cmpq_vzx35$@o6oas*t6hqt8cn16+8zpk9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
